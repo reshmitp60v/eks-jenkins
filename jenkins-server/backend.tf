f7cb594d9905b6338fb8afe29de9917000b2e11f
Reshmi T <reshmit@lentra.ai>
2025-04-21 19:14:12 +0530

    first commit

--- jenkins-server/backend.tf
+++ jenkins-server/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mytodoappbucket"
-    key    = "jenkins/terraform.tfstate"
-    region = "us-east-2"
+    key    = "eks/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
